a7ddc999883d0f4d5c85ef062d525f0b46a30ee5
Lenovo <Lenovo@LAPTOP-Q8UA51BI>
2022-08-25 13:15:46 +0530

    Jira 1212 Description  project pm

---
+++ Encapsulation/src/HidingData/accessible.java
@@ -0,0 +1,20 @@
+package HidingData;
+
+
+public class accessible extends masteclass{
+
+	public static void main(String[] args) {
+		accessible obj=new accessible();
+		obj.set(70000);
+		System.out.println(obj.get());
+	}
+
+
+
+
+
+
+
+
+
+}
---
+++ Encapsulation/src/HidingData/masteclass.java
@@ -0,0 +1,17 @@
+package HidingData;
+
+public class masteclass {
+       private int a=50000;
+
+public int get()
+{
+	return a;
+}
+public void set(int b)
+{
+	this.a=b;
+}
+
+
+
+}
